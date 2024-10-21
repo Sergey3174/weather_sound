3d7dc50402f15f645836ac403e98b5b0505d3abb
Sergey <skauzerlfc@gmail.com>
2024-10-21 05:54:44 +0300

    финал

--- src/main.ts
+++ src/main.ts
@@ -1,137 +1,135 @@
-import './style.css'
-import { WEATHER } from './weather'
-import pauseIcon from './assets/icons/pause.svg'
+import './style.css';
+import { WEATHER, WeatherItem } from './weather';
+import pauseIcon from './assets/icons/pause.svg';
 
 class App {
-  currentWeather = {};
-  weather;
-  appContainer;
-  audioPlayer;
+    currentWeather: WeatherItem = {} as WeatherItem;
+    weather: WeatherItem[];
+    appContainer: HTMLDivElement | null = null
+    audioPlayer: HTMLAudioElement;
+    volumeLevel: string = '50'
 
-  constructor(weather) {
-      this.weather = weather;
-      this.audioPlayer = document.createElement('audio');
-      this.audioPlayer.id = 'audio-player'; 
-      document.body.appendChild(this.audioPlayer);
-  }
 
-  setCurrentWeather(id) {
-      this.currentWeather = this.weather.find(elem => elem.id === id) || {};
-      console.log(this.currentWeather);
-      this.render();
-  }
+    constructor(weather: WeatherItem[]) {
+        this.weather = weather;
+        this.audioPlayer = document.createElement('audio');
+        this.audioPlayer.id = 'audio-player';
+        this.audioPlayer.addEventListener('ended', () => {
+            this.audioPlayer.currentTime = 0
+            this.audioPlayer.play();          
+        });
+        document.body.appendChild(this.audioPlayer);
+    }
 
-  render() {
-      if (this.appContainer) {
-          this.appContainer.innerHTML = '';
-      } else {
-          this.appContainer = this.createApp();
-      }
+    setCurrentWeather(id: string) {
+        this.currentWeather = this.weather.find((elem) => elem.id === id) || ({} as WeatherItem);
+        this.render();
+    }
 
-      this.appContainer.append(this.createBackground(), this.createButtonsBlock());
-      return this.appContainer;
-  }
-
-  createApp() {
-      const appContainer = document.createElement('div');
-      appContainer.classList.add('app-container');
-      return appContainer;
-  }
-
-  createBackground() {
-      const appBackground = document.createElement('img');
-      appBackground.classList.add('background');
-      appBackground.src = this.currentWeather.img || '';
-      appBackground.style.visibility = this.currentWeather.img ? 'visible' : 'hidden';
-
-      if (!this.currentWeather.img) {
-          const noDataMessage = document.createElement('p');
-          noDataMessage.textContent = 'Данные о погоде отсутствуют.';
-          appBackground.appendChild(noDataMessage);
-      }
-
-      return appBackground;
-  }
-
-  createVolumeControl() {
-      const volumeContainer = document.createElement('div');
-      volumeContainer.classList.add('volume-control');
-
-      const inputVolume = document.createElement('input');
-      inputVolume.type = 'range';
-      inputVolume.id = 'volume';
-      inputVolume.min = 0;
-      inputVolume.max = 100;
-      inputVolume.value = 50;
-      volumeContainer.append(inputVolume);
-
-      this.audioPlayer.volume = inputVolume.value / 100;
-
-      inputVolume.addEventListener('input', (e) => {
-          this.audioPlayer.volume = e.target.value / 100;
-          console.log(this.audioPlayer.volume);
-      });
-
-      return volumeContainer;
-  }
-
-  createButtonsBlock() {
-      const buttonsWrapper = document.createElement('div');
-      buttonsWrapper.classList.add('buttons__wrapper');
-
-      const title = document.createElement('h1');
-      title.textContent = 'Weather Sound';
-
-      const buttonContainer = document.createElement('div');
-      buttonContainer.classList.add('buttons__container');
-
-      this.weather.forEach(elem => buttonContainer.append(this.createButton(elem)));
-
-      buttonsWrapper.append(title, buttonContainer, this.createVolumeControl());
-      return buttonsWrapper;
-  }
-
-  createButton({ id, name, img, icon }) {
-      const button = document.createElement('button');
-      const buttonBg = document.createElement('img');
-      const buttonIcon = document.createElement('img');
-
-      buttonBg.classList.add('background-button');
-      buttonIcon.classList.add('icon-button');
-
-      buttonBg.src = img;
-      buttonIcon.src = icon;
-
-      button.id = id;
-      button.name = name;
-      button.append(buttonBg, buttonIcon);
-
-      button.addEventListener('click', this.handleClick.bind(this, id, buttonIcon));
-      return button;
-  }
-
-  handleClick(id, buttonIcon) {
-
-      
-      if (id === this.currentWeather.id) {
-        if(this.audioPlayer.paused){
-          this.audioPlayer.play()
-          buttonIcon.src = pauseIcon
+    render(): HTMLElement {
+        if (this.appContainer) {
+            this.appContainer.innerHTML = '';
         } else {
-          buttonIcon.src = this.currentWeather.icon
-          this.audioPlayer.pause();
+            this.appContainer = this.createAppContainer();
         }
-        return
-      }
 
-      this.setCurrentWeather(id);
-      this.audioPlayer.src = this.currentWeather.audio || '';
-      const newIcon = document.getElementById(id)?.lastElementChild
-      newIcon.src = pauseIcon
-      this.audioPlayer.play()
-  }
+        this.appContainer.append(this.createBackground(), this.createButtonsBlock());
+        return this.appContainer;
+    }
+
+    createAppContainer(): HTMLDivElement {
+        const appContainer = document.createElement('div');
+        appContainer.classList.add('app-container');
+        return appContainer;
+    }
+
+    createBackground(): HTMLImageElement {
+        const appBackground = document.createElement('img');
+        appBackground.classList.add('background');
+        appBackground.src = this.currentWeather.img || '';
+        appBackground.style.visibility = this.currentWeather.img ? 'visible' : 'hidden';
+
+        return appBackground;
+    }
+
+    createVolumeControl(): HTMLDivElement {
+        const volumeContainer = document.createElement('div');
+        volumeContainer.classList.add('volume-control');
+
+        const inputVolume = document.createElement('input');
+        inputVolume.type = 'range';
+        inputVolume.id = 'volume';
+        inputVolume.min = '0';
+        inputVolume.max = '100';
+        inputVolume.value = this.volumeLevel;
+        volumeContainer.append(inputVolume);
+
+        this.audioPlayer.volume = parseInt(inputVolume.value, 10) / 100;
+
+        inputVolume.addEventListener('input', this.changeVolume.bind(this))
+
+        return volumeContainer;
+    }
+
+    createButtonsBlock(): HTMLDivElement {
+        const buttonsWrapper = document.createElement('div');
+        buttonsWrapper.classList.add('buttons__wrapper');
+
+        const title = document.createElement('h1');
+        title.textContent = 'Weather Sound';
+
+        const buttonContainer = document.createElement('div');
+        buttonContainer.classList.add('buttons__container');
+
+        this.weather.forEach((elem) => buttonContainer.append(this.createButton(elem)));
+
+        buttonsWrapper.append(title, buttonContainer, this.createVolumeControl());
+        return buttonsWrapper;
+    }
+
+    createButton({ id, name, img, icon }: WeatherItem): HTMLButtonElement {
+        const button = document.createElement('button');
+        const buttonBg = document.createElement('img');
+        const buttonIcon = document.createElement('img');
+
+        buttonBg.classList.add('background-button');
+        buttonIcon.classList.add('icon-button');
+
+        buttonBg.src = img;
+        buttonIcon.src = this.currentWeather.id === id ? pauseIcon : icon;
+
+        button.id = id;
+        button.name = name;
+        button.append(buttonBg, buttonIcon);
+
+        button.addEventListener('click', () => this.handleClick(id, buttonIcon));
+        return button;
+    }
+
+    handleClick(id: string, buttonIcon: HTMLImageElement) {
+        if (id === this.currentWeather.id) {
+            if (this.audioPlayer.paused) {
+                this.audioPlayer.play();
+                buttonIcon.src = pauseIcon;
+            } else {
+                buttonIcon.src = this.currentWeather.icon;
+                this.audioPlayer.pause();
+            }
+            return;
+        }
+
+        this.setCurrentWeather(id);
+        this.audioPlayer.src = this.currentWeather.audio || '';
+        this.audioPlayer.play();
+    }
+
+    changeVolume (event: Event) {
+        this.audioPlayer.volume = parseInt((event.target as HTMLInputElement).value, 10) / 100;
+        this.volumeLevel = (event.target as HTMLInputElement).value
+    }
 }
 
 const app = new App(WEATHER);
-const container = document.querySelector('#app');
+const container = document.querySelector('#app') as HTMLElement;
 container?.append(app.render());
+
--- src/weather.ts
+++ src/weather.ts
@@ -1,33 +1,42 @@
-import rainBackground from './assets/rainy-bg.jpg'
-import summerBackground from './assets/summer-bg.jpg'
-import winterBackground from './assets/winter-bg.jpg'
-import iconRain from './assets/icons/cloud-rain.svg'
-import iconWinter from './assets/icons/cloud-snow.svg'
-import iconSummer from './assets/icons/sun.svg'
-import rainAudio from './assets/sounds/rain.mp3'
-import sumerAudio from './assets/sounds/summer.mp3'
-import winterAudio from './assets/sounds/winter.mp3'
+import rainBackground from './assets/rainy-bg.jpg';
+import summerBackground from './assets/summer-bg.jpg';
+import winterBackground from './assets/winter-bg.jpg';
+import iconRain from './assets/icons/cloud-rain.svg';
+import iconWinter from './assets/icons/cloud-snow.svg';
+import iconSummer from './assets/icons/sun.svg';
+import rainAudio from './assets/sounds/rain.mp3';
+import summerAudio from './assets/sounds/summer.mp3';
+import winterAudio from './assets/sounds/winter.mp3';
 
-export const WEATHER = [
+// Определяем интерфейс для элемента погоды
+export interface WeatherItem {
+    id: string;
+    name: string;
+    img: string;
+    audio: string;
+    icon: string;
+}
+
+export const WEATHER: WeatherItem[] = [
     {
-      id: '001',
-      name: 'rain',
-      img: rainBackground,
-      audio: rainAudio,
-      icon: iconRain
+        id: '001',
+        name: 'rain',
+        img: rainBackground,
+        audio: rainAudio,
+        icon: iconRain
     },
     {
-      id: '002',
-      name: 'summer',
-      img: summerBackground,
-      audio: sumerAudio,
-      icon: iconSummer
+        id: '002',
+        name: 'summer',
+        img: summerBackground,
+        audio: summerAudio,
+        icon: iconSummer
     },
     {
-      id: '003',
-      name: 'winter',
-      img: winterBackground,
-      audio: winterAudio,
-      icon: iconWinter
+        id: '003',
+        name: 'winter',
+        img: winterBackground,
+        audio: winterAudio,
+        icon: iconWinter
     }
-  ]
+];
